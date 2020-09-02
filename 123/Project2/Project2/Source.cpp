5a198179c8b2aea5355c3e83ca69d2199fcd9088
Dmitriy24687 <70640479+Dmitriy24687@users.noreply.github.com>
2020-09-02 15:38:37 +0300

    Add files via upload

---
+++ 123/Project2/Project2/Source.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+void main()
+{
+	char a, b, c, d, e;
+	int a1, b1, c1, d1, e1, h, h1, h2, m;
+	a = Switzerland;
+	b = Belgium;
+	c = Ukraine;
+	d = America;
+	e = Canada;
+
+	std:: cout <<"Ответье на вопрос">>;
+	std:: cout <<"Европа или Америка (1или2)">>;
+
+	if h = 1
+		a1 + ; b1 + ; c1 + ;
+	else d1 + ; e1 + ;
+
+	std:: cout << "Важен ли уровень жизни (1да или 2нет)" >> ;
+
+	if h1 = 1
+		a1 + ; b1 + ; d1 + ;
+	else c1 + ; e1 + ;
+
+	std::cout << "Вы пьете? (1да или 2нет)" >> ;
+
+	if h2 = 1
+		e1++; b1 + ; 
+
+	if 
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
